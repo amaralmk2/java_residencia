39781a871fc3c715e9c12f54bf3a8fbdf5dca5c2
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2023-11-25 19:29:01 -0300

    Feito segunda parte do exercicio, Criado arquivo e classe JogoAdvinhacao

---
+++ P002/src/JogoAdvinhacao.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class JogoAdvinhacao {
+    private int numeroAleatorio;
+    private int tentativas;
+
+    public JogoAdvinhacao() {
+        Random random = new Random();
+        numeroAleatorio = random.nextInt(100) + 1;
+        tentativas = 0;
+    }
+
+    public void iniciarJogo(Scanner scanner) {
+        int palpite;
+
+        System.out.println("Tente adivinhar o número entre 1 e 100.");
+
+        do {
+            System.out.print("Digite seu palpite: ");
+            palpite = scanner.nextInt();
+            tentativas++;
+
+            if (palpite > numeroAleatorio) {
+                System.out.println("Muito alto. Tente novamente.");
+            } else if (palpite < numeroAleatorio) {
+                System.out.println("Muito baixo. Tente novamente.");
+            } else {
+                System.out.println("Parabéns! Você acertou em " + tentativas + " tentativas.");
+            }
+
+        } while (palpite != numeroAleatorio);
+    }
+
+
+
+
+    public static void main(String[] args) {
+      
+    }
+}
